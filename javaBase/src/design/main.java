57b03d9485f8c39315b97c1072ac3fa22a1d2f64
shushish <770484183@qq.com>
2018-10-19 17:34:19 +0800

    single

--- javaBase/src/design/main.java
+++ javaBase/src/design/main.java
@@ -4,10 +4,8 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Single.getSingleObj());
-		System.out.println(Single.getSingleObj());
-		System.out.println(Single.getSingleObj());
-		System.out.println(Single.getSingleObj());
+		System.out.println(Single.getInstance().toString());
+
 	}
 
 }
